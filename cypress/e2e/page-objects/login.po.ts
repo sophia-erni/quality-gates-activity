f77ec967d36bc9c38aaa896b4020913655b76a88
Jardeleza Sophia Gabrielle (External) <sophia.jardeleza@betterask.erni>
2025-03-20 16:16:36 +0800

    update

--- cypress/e2e/page-objects/login.po.ts
+++ cypress/e2e/page-objects/login.po.ts
@@ -6,8 +6,8 @@ export class LoginPage {
   }
 
   login(username: string, password: string) {
-    cy.get('input[name="username"]', { timeout: 10000 }).should('be.visible').type(username); // Increase the timeout to 10 seconds
-    cy.get('input[name="password"]', { timeout: 10000 }).should('be.visible').type(password); // Increase the timeout to 10 seconds
-    cy.get('button[type="submit"]', { timeout: 10000 }).should('be.visible').click(); // Increase the timeout to 10 seconds
+    cy.get('input[name="username"]', { timeout: 20000 }).should('be.visible').type(username); // Increase the timeout to 10 seconds
+    cy.get('input[name="password"]', { timeout: 20000 }).should('be.visible').type(password); // Increase the timeout to 10 seconds
+    cy.get('button[type="submit"]', { timeout: 20000 }).should('be.visible').click(); // Increase the timeout to 10 seconds
   }
 }
