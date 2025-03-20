69eae1b4eb3c23da88a70d86e876132efa73c902
Jardeleza Sophia Gabrielle (External) <sophia.jardeleza@betterask.erni>
2025-03-20 16:26:38 +0800

    update

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -15,9 +15,4 @@ describe('when the app loads', () => {
     cy.url().should('include', login.url);
     login.login();
   });
-
-  // it('should display the home page and say hello', () => {
-  //   cy.url().should('include', home.url);
-  //   home.welcomeText.contains('Hello world !');
-  // });
 });
