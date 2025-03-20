67e76fa763ff842b10174660bb6a9fe3a896b30e
Jardeleza Sophia Gabrielle (External) <sophia.jardeleza@betterask.erni>
2025-03-20 15:52:48 +0800

    update

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,10 +1,19 @@
+import { LoginPage } from './page-objects/login.po';
+import { AppSharedPage } from './page-objects/app-shared.po';
 import { HomePage } from './page-objects/home.po';
 
-describe('Home Page', () => {
+describe('when the app loads', () => {
+  const login = new LoginPage();
+  const app = new AppSharedPage();
   const homePage = new HomePage();
 
-  beforeEach(() => {
-    homePage.navigateTo();
+  before(() => {
+    app.navigateAndSetLanguage();
+  });
+
+  it('should display the login page and log in', () => {
+    cy.url().should('include', login.url);
+    login.login();
   });
 
   it('should display the home page and say hello', () => {
