2907eb4bd02d8a0818b6c7cdcf1b73356dcb82ac
Jardeleza Sophia Gabrielle (External) <sophia.jardeleza@betterask.erni>
2025-03-20 14:03:41 +0800

    update

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   screenshotsFolder: 'cypress/screenshots',
   fixturesFolder: 'cypress/fixtures',
   projectId: 'd6gihf',
+  defaultCommandTimeout: 10000, // Increase the default command timeout to 10 seconds
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
